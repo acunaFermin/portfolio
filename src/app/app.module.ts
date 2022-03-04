12dd6be5af7560b7e7c622060033aa65d8230ec8
Fermin <fermin.unicen@gmail.com>
2022-03-04 10:00:40 -0300

    portada con lampara de lava

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BarraLateralComponent } from './barra-lateral/barra-lateral.component';
+import { CuerpoComponent } from './cuerpo/cuerpo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BarraLateralComponent
+    BarraLateralComponent,
+    CuerpoComponent
   ],
   imports: [
     BrowserModule,
